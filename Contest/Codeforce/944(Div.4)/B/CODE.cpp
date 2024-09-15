a69d8185413ba23a85b03cd7d815b492dbbc0d18
AllenYGY <allenygy@outlook.com>
2024-09-16 02:06:53 +0800

    Update

---
+++ Contest/Codeforce/944(Div.4)/B/CODE.cpp
@@ -0,0 +1,50 @@
+// #include<bits/stdc++.h>
+#include<iostream>
+#define int long long
+#define endl '\n'
+using namespace std;
+ 
+int read(){
+    int x = 0, f = 1;
+    char ch = getchar();
+    while (ch < '0' || ch > '9'){
+        if (ch == '-')
+            f = -1;
+        ch = getchar();
+    }
+    while (ch >= '0' && ch <= '9')
+        x = x * 10 + ch - '0', ch = getchar();
+    return x * f;
+}
+ 
+void write(int x){
+    if (x < 0)
+        putchar('-'), x = -x;
+    if (x > 9)
+        write(x / 10);
+    putchar(x % 10 + '0');
+}
+ 
+void solve(){ 
+    string s;
+    cin >> s;
+    for(int i=1;i<s.size();i++){
+        if(s[i]!=s[i-1]){
+            char a = s[i-1];
+            s[i-1] = s[i]; 
+            s[i] = a;
+            cout<<"YES"<<endl;
+            cout << s << endl;
+            return;
+        }
+    }
+    cout<<"NO"<<endl;
+}
+ 
+signed main() {
+    int T = read();
+    while(T--){
+        solve();
+    }
+    return 0;
+}
---
+++ Contest/Codeforce/944(Div.4)/D/CODE.cpp
@@ -0,0 +1,53 @@
+// #include<bits/stdc++.h>
+#include<iostream>
+#define int long long
+#define endl '\n'
+using namespace std;
+ 
+int read(){
+    int x = 0, f = 1;
+    char ch = getchar();
+    while (ch < '0' || ch > '9'){
+        if (ch == '-')
+            f = -1;
+        ch = getchar();
+    }
+    while (ch >= '0' && ch <= '9')
+        x = x * 10 + ch - '0', ch = getchar();
+    return x * f;
+}
+ 
+void write(int x){
+    if (x < 0)
+        putchar('-'), x = -x;
+    if (x > 9)
+        write(x / 10);
+    putchar(x % 10 + '0');
+}
+ 
+void solve(){ 
+    string s;
+    cin>>s;
+    // 01
+    // 11
+    int ans=1;
+    int flag=0;
+    for(int i=1;i<s.size();i++){
+        if(s[i-1]!=s[i]){
+            if(s[i-1]=='0' && flag==0){
+                flag=1;
+            }else{
+               ans+=1; 
+            }
+        }
+    }
+    cout<<ans<<endl;
+}
+ 
+signed main() {
+    int T = read();
+    while(T--){
+        solve();
+    }
+    return 0;
+}

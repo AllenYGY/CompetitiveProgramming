b281b8c9594ee7f8eaaddc68715dd0b3bd3cd6f7
AllenYGY <allenygy@outlook.com>
2024-09-23 11:08:10 +0800

    Update

---
+++ Contest/ATCODER/343/A/CODE.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#define int long long
+#define endl '\n'
+using namespace std;
+ 
+int read(){
+    int x = 0, f = 1;
+    char ch = getchar();
+    while (ch < '0' || ch > '9'){
+        if (ch == '-')
+            f = -1;
+        ch = getchar();
+    }
+    while (ch >= '0' && ch <= '9')
+        x = x * 10 + ch - '0', ch = getchar();
+    return x * f;
+}
+ 
+void write(int x){
+    if (x < 0)
+        putchar('-'), x = -x;
+    if (x > 9)
+        write(x / 10);
+    putchar(x % 10 + '0');
+}
+ 
+void solve(){
+       
+}
+ 
+signed main() {
+    int a=read(),b=read();
+    for(int i=0;i<=9;i++) {
+        if(i!=a+b) {
+            cout<<i<<endl;
+            break;
+        }
+    }
+    return 0;
+}
